3ac34facf032bfda1074b2855d066cb2422ff3d1
Bowen Chan <Bowenmchan@gmail.com>
2016-05-06 09:05:13 -0700

    Fixed some sql statement reading

--- index.php
+++ index.php
@@ -11,15 +11,42 @@
 <body>
 	<?php
 		#$link = mysqli_con
-		$query = mysqli_query($dbc, "SELECT * FROM `". $product . "` WHERE `package_size` = '6 oz'");
+		/**$query2 = mysqli_query($dbc, "SELECT
+	`description`,
+	`city`,
+    `day_of_week`,
+    `store_county`,
+    sum(Sales.dollar_sales) AS `Dollar_Sales`
+    FROM
+    `". $product ."`" . "as P,
+   	 `".  $store . "`" . "as S,
+    `". $time . "`" . "as T,
+    `" . $salesFact ."` as Sales
+	WHERE
+	
+	Sales.product_key = P.product_key AND
+    Sales.store_key = S.store_key AND
+    T.time_key AND Sales.time_key 
+    
+    
+GROUP BY
+	P.description,
+    S.city,
+    S.store_county,
+    T.day_of_week
+    ");*/
+
+    	//Checks if there are any data inside the table
+		$query = mysqli_query($dbc, "SELECT COUNT(*) FROM `". $product . "`");
 
 		$row = mysqli_fetch_array($query, MYSQLI_NUM);
-
+	
 		if($row[0] != 0){
-
-			while($row = mysqli_fetch_array($query, MYSQLI_ASSOC)){
+			//selects a specific keyword to access
+			$query = mysqli_query($dbc, "SELECT product_key FROM `". $product . "`");
+			while($row = mysqli_fetch_assoc($query)){
 				echo "<p>" . $row['product_key'] . "</p>";
-			}
+			}	
 		}
 	?>
 
