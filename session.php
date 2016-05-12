f548f766bcfaf22ef45be6c6d9cf124e83c7a532
Bowen Chan <Bowenmchan@gmail.com>
2016-05-11 19:08:50 -0700

    Fixed ER Diagram

--- session.php
+++ session.php
@@ -10,7 +10,7 @@
 	    $_SESSION['product'] = 7;
 	    $_SESSION['store'] = 4;
 	    $_SESSION['promotion'] = 1;
-	    $_SESSION['timeArray'] = array("date", "day_of_week", "day_number_in_month", "week_number_in_year", "week_number_overall","Month", "quarter", "fiscal_period", "year");
+	    $_SESSION['timeArray'] = array("date", "day_of_week", "day_number_in_month", "week_number_in_year", "week_number_overall","month", "quarter", "fiscal_period", "year");
 	    $_SESSION['productArray'] = array("description", "full_description", "sku_number", "package_size", "brand", "subcategory", "category", "department", "package_type", "diet_type");
 	    $_SESSION['storeArray'] = array("name", "store_number","store_street_address", "city", "store_county", "store_state", "store_zip","sales_district", "sales_region");
 	    $_SESSION['promotionArray'] = array("promotion_name","price_reduction_type","ad_type", "display_type", "coupon_type", "ad_media","display_provider");
